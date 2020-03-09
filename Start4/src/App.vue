124f1070f7a990f044b2d54dcd24b6baa65fa2af
Junya Nishiwaki <junya4318629@yahoo.co.jp>
2020-03-10 07:51:35 +0800

    a

--- Start4/src/App.vue
+++ Start4/src/App.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="container">
-    
+      <appQuoteGrid :quotes="quotes"></appQuoteGrid>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import QuoteGrid from './components/QuoteGrid.vue'
+
+export default {
+  data() {
+    return {
+      quotes: [
+        'Just a Quote to see something'
+      ],
+      maxQuotes: 10
     }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid
+  }
+}
 </script>
 
 <style>
