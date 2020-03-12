330269d22af84ad39847f4a574c2a265cb4c88ab
Junya Nishiwaki <junya.nishiwaki@fullspeedtechnologies.com>
2020-03-12 17:55:15 +0800

    a

--- Section Code (Start5)/src/main.js
+++ Section Code (Start5)/src/main.js
@@ -3,8 +3,19 @@ import App from './App.vue'
 
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
-    el.style.backgroundColor = 'green'
-    
+    // el.style.backgroundColor = 'green'
+    // el.style.backgroundColor = binding.value
+    let delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000
+    }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.background = binding.value
+      } else {
+        el.style.color = binding.value
+      }
+    }, delay)
   }
 })
 new Vue({
---
+++ Start5/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-customOn:click="clicked" class="btn btn-primary">Click me</button>
+                <hr>
+                <div style="width:100px;height:100px;background:lightgreen" v-customOn:mouseenter="mouseEnter" v-customOn:mouseleave="mouseLeave">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    customOn: {
+      bind(el, binding, vnode) {
+        // el.onclick = () => {
+        //   binding.value()
+        // }
+        const type = binding.arg,
+              fn = binding.value
+        el.addEventListener(type, fn)
+      }
+    }
+  },
+  methods: {
+    clicked() {
+      alert('I was clicked!')
+    },
+    mouseEnter() {
+      console.log('mouse entered!')
+    },
+    mouseLeave() {
+      console.log('Mouse leaved!')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
